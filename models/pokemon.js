778ce6e153fd8c03e3431c266064ca3baf4fc41a
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-08 14:17:19 -0500

    corrected error in types key

--- models/pokemon.js
+++ models/pokemon.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       image: DataTypes.STRING,
       types: {
         allowNull: false,
-        type: DataTypes.ARRAY(Sequelize.STRING)
+        type: DataTypes.ARRAY(DataTypes.STRING)
       }
     },
     {
