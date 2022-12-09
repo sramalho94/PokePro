3972ae2a62ae560af9aca5067cedcde5e2fb4371
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-09 10:05:00 -0500

    created routes for user methods

---
+++ routes/UserRouter.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const controller = require('../controllers/UserController')
+const middleware = require('../middleware')
+
+router.get('/', controller.GetUsers)
+
+router.post('/login', controller.LoginUser)
+router.post('/register', controller.RegisterUser)
+
+router.post(
+  '/update',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+)
+
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
+
+router.delete(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteUser
+)
+
+router.get('/:id', controller.GetUserById)
+module.exports = router
