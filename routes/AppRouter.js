f59cb88d31edcf3b8742033edb0bed46b3a2896d
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-10 12:44:31 -0500

    imported trainer routes to app router

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const PokemonRouter = require('./PokemonRouter')
+const TrainerRouter = require('./TrainerRouter')
 
 router.use('/users', UserRouter)
 router.use('/pokemon', PokemonRouter)
+router.use('/trainers', TrainerRouter)
+
 module.exports = router
