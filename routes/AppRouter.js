775e54a8db98acffde83d67741f6569a5c328253
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-11 12:54:11 -0500

    imported team routes to App Router

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const PokemonRouter = require('./PokemonRouter')
 const TrainerRouter = require('./TrainerRouter')
+const TeamRouter = require('./TeamRouter')
 
 router.use('/users', UserRouter)
 router.use('/pokemon', PokemonRouter)
 router.use('/trainers', TrainerRouter)
+router.use('/teams', TeamRouter)
 
 module.exports = router
