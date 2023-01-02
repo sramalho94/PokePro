8ec0954820ea5f561f53a8a9647dbb85402d45eb
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-02 17:28:08 -0500

    changes to user router

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -7,7 +7,7 @@ router.get('/', controller.GetUsers)
 router.post('/login', controller.LoginUser)
 router.post('/register', controller.RegisterUser)
 
-router.put(
+router.post(
   '/update',
   // middleware.stripToken,
   // middleware.verifyToken,
