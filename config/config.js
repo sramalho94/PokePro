eec7b1d3c21bdf2d9ee03934070e25773cd1af48
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-17 12:59:44 -0400

    changes to bcrypt package

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 module.exports = {
   development: {
-    database: 'pokepro_development',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres'
   },
   test: {
